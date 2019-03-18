d0a7dd77ebca6d010ccd26a9119fb77a331a7d7f
Ruslan Molchaniuk <ram14apple@gmail.com>
2019-03-18 20:22:15 +0100

    implemented splash screen

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ProgressBarAndroid,
+  Dimensions
+} from "react-native";
+import { robotoWeights } from "react-native-typography";
+import { ScreenOrientation } from "expo";
 
-export default class App extends React.Component {
+export default class Menu extends React.Component {
+  componentDidMount() {
+    ScreenOrientation.allowAsync(
+      ScreenOrientation.Orientation.ALL_BUT_UPSIDE_DOWN
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Image style={styles.label} source={require("./assets/label.png")} />
+        <Text style={[styles.name, styles.robotoWeights]}>TicTacToe</Text>
+        <ProgressBarAndroid
+          style={styles.progressBar}
+          styleAttr="Horizontal"
+          indeterminate={true}
+          progress={0.5}
+          color={"black"}
+        />
       </View>
     );
   }
@@ -13,9 +36,26 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
+    marginBottom: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "space-between"
   },
+  label: {
+    height: 250,
+    width: 250
+  },
+  name: {
+    color: "black",
+    fontSize: 70
+  },
+  robotoWeights: {
+    ...robotoWeights.titleObject,
+    ...robotoWeights.light
+  },
+  progressBar: {
+    width: Dimensions.get("window").width / 2
+  }
 });
